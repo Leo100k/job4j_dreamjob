15fb74998156e77138bd23cc25e639fb6bd0aeb0
Leo100k <lemail2004@mail.ru>
2024-06-04 18:56:14 +0300

    1. Генератор HTML [#504838]

--- src/main/java/dreamjob/controller/IndexController.java
+++ src/main/java/dreamjob/controller/IndexController.java
@@ -1,13 +1,14 @@
 package dreamjob.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Spring Spring Hello World!";
+        return "index";
     }
 
 }
