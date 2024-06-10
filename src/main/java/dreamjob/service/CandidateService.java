0403a19abe2e5ef02fc1b6aeae5d28410f743dcd
Leo100k <lemail2004@mail.ru>
2024-06-10 13:39:25 +0300

    1. Слоеная архитектура. [#504851]

---
+++ src/main/java/dreamjob/service/CandidateService.java
@@ -0,0 +1,18 @@
+package dreamjob.service;
+import dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CandidateService {
+
+    Candidate save(Candidate candidate);
+
+    boolean deleteById(int id);
+
+    boolean update(Candidate candidate);
+
+    Optional<Candidate> findById(int id);
+
+    Collection<Candidate> findAll();
+}
---
+++ src/main/java/dreamjob/service/SimpleCandidateService.java
@@ -0,0 +1,46 @@
+package dreamjob.service;
+import dreamjob.model.Candidate;
+import dreamjob.model.Vacancy;
+import dreamjob.repository.MemoryCandidateRepository;
+import dreamjob.repository.CandidateRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class SimpleCandidateService implements CandidateService {
+
+    private static final SimpleCandidateService INSTANCE = new SimpleCandidateService();
+
+    private final CandidateRepository candidateRepository = MemoryCandidateRepository.getInstance();
+
+    private SimpleCandidateService() { }
+
+    public static SimpleCandidateService getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        return candidateRepository.save(candidate);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return candidateRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidateRepository.update(candidate);
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return candidateRepository.findById(id);
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidateRepository.findAll();
+    }
+}
