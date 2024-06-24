6f29d698ecb3038f5bb8e93d49785f820dd83a68
Leo100k <lemail2004@mail.ru>
2024-06-24 16:50:21 +0300

    1. Страница login.html [#504863]

--- src/main/java/dreamjob/controller/UserController.java
+++ src/main/java/dreamjob/controller/UserController.java
@@ -62,43 +62,4 @@ public class UserController {
         return "redirect:/vacancies";
     }
 
-    /*
-    @GetMapping("/{id}")
-    public String getById(Model model, @PathVariable int id) {
-        var vacancyOptional = vacancyService.findById(id);
-        if (vacancyOptional.isEmpty()) {
-            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
-            return "errors/404";
-        }
-        model.addAttribute("cities", cityService.findAll());
-        model.addAttribute("vacancy", vacancyOptional.get());
-        return "vacancies/one";
-    }
-
-    @PostMapping("/update")
-    public String update(@ModelAttribute Vacancy vacancy, @RequestParam MultipartFile file, Model model) {
-        try {
-            var isUpdated = vacancyService.update(vacancy, new FileDto(file.getOriginalFilename(), file.getBytes()));
-            if (!isUpdated) {
-                model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
-                return "errors/404";
-            }
-            return "redirect:/vacancies";
-        } catch (Exception exception) {
-            model.addAttribute("message", exception.getMessage());
-            return "errors/404";
-        }
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(Model model, @PathVariable int id) {
-        var isDeleted = vacancyService.deleteById(id);
-        if (!isDeleted) {
-            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
-            return "errors/404";
-        }
-        return "redirect:/vacancies";
-    }
-
- */
 }
